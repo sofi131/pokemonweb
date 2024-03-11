0aa297dae2416af4868fa706aa9ba3ffac58af5c
sofi131 <sofiasanmartin131@gmail.com>
2024-03-11 19:31:12 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.onload=()=>{
+    let menu=document.getElementById("barras-menu");
+
+    menu.onclick=()=>{
+        // objeto - método classList - propiedad ya que no tiene paréntesis
+        // contains es un método que contiene el objeto menú móvil
+        if(document.getElementById("menu-movil").classList.contains("menu-movil")){
+            document.getElementById("menu-movil").classList.remove("menu-movil");
+        }else{
+            document.getElementById("menu-movil").classList.add("menu-movil");
+        }
+    }
+}
