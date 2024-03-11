69c149ee2f81d82c5a4db4179e85251ef98eb127
sofi131 <sofiasanmartin131@gmail.com>
2024-03-11 19:52:21 +0100

    Cambios estilo

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-window.onload=()=>{
-    let menu=document.getElementById("barras-menu");
+window.onload = () => {
+    let menu = document.getElementById("barras-menu");
 
-    menu.onclick=()=>{
+    menu.onclick = () => {
         // objeto - método classList - propiedad ya que no tiene paréntesis
         // contains es un método que contiene el objeto menú móvil
-        if(document.getElementById("menu-movil").classList.contains("menu-movil")){
+        if (document.getElementById("menu-movil").classList.contains("menu-movil")) {
             document.getElementById("menu-movil").classList.remove("menu-movil");
-        }else{
+        } else {
             document.getElementById("menu-movil").classList.add("menu-movil");
         }
     }
