228b808f4148f4fd7c864ca0b99bb8edea42a260
sofi131 <sofiasanmartin131@gmail.com>
2024-03-11 20:37:54 +0100

    Fetch

--- js/index.js
+++ js/index.js
@@ -10,4 +10,23 @@ window.onload = () => {
             document.getElementById("menu-movil").classList.add("menu-movil");
         }
     }
-}
+    //solicitar primeros pokemon
+        //Solicitar primeros pokemon
+        let url = "https://pokeapi.co/api/v2/pokemon";
+    //mostramos loading
+    document.getElementById("loading").style.display="block";
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                document.getElementById("loading").style.display="none";
+                console.log(data); // Aquí puedes trabajar con los datos de respuesta
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
